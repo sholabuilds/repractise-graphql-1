ea1dfd2c0be6f672b7620387433210d79eb1af15
codewurld <sholaqdev@gmail.com>
2023-05-09 16:05:56 +0100

    sets up schema and server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,14 @@
 //TODO
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone")
+const typeDefs = require("./schema");
+
+const startApolloServer = async () => {
+    // instantiate ApolloServer
+    const server = new ApolloServer({ typeDefs });
+    // starts the server
+    const { url } = await startStandaloneServer(server);
+    console.log(`Server is running! Query at ${url}`);
+}
+
+startApolloServer();
