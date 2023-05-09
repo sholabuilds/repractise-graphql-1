3e75a04baddcdc90c5a9b9d58a77ae4424ce2919
codewurld <sholaqdev@gmail.com>
2023-05-09 17:15:06 +0100

    set up apollo client

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  // specifies GraphQL server location
+  uri: 'http://localhost:4000',
+  // caches results to reduce network requests
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
+    {/* Apollo Provider */}
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
